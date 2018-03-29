575cf210b567c9bdbe0c37320da31710c4aab912
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-29 15:58:57 -0500

    oclif init

---
+++ README.md
@@ -0,0 +1,17 @@
+with-piping
+===========
+
+
+
+[![Version](https://img.shields.io/npm/v/with-piping.svg)](https://npmjs.org/package/with-piping)
+[![CircleCI](https://circleci.com/gh/JuanCaicedo/with-piping/tree/master.svg?style=shield)](https://circleci.com/gh/JuanCaicedo/with-piping/tree/master)
+[![Appveyor CI](https://ci.appveyor.com/api/projects/status/github/JuanCaicedo/with-piping?branch=master&svg=true)](https://ci.appveyor.com/project/JuanCaicedo/with-piping/branch/master)
+[![Codecov](https://codecov.io/gh/JuanCaicedo/with-piping/branch/master/graph/badge.svg)](https://codecov.io/gh/JuanCaicedo/with-piping)
+[![Downloads/week](https://img.shields.io/npm/dw/with-piping.svg)](https://npmjs.org/package/with-piping)
+[![License](https://img.shields.io/npm/l/with-piping.svg)](https://github.com/JuanCaicedo/with-piping/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import {Command, flags} from '@oclif/command'
+
+class WithPiping extends Command {
+  static description = 'describe the command here'
+
+  static examples = [
+    `$ with-piping
+hello world from ./src/with-piping.ts!
+`,
+  ]
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    // add --help flag to show CLI version
+    help: flags.help({char: 'h'}),
+
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({char: 'n', description: 'name to print'}),
+    force: flags.boolean({char: 'f'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  async run() {
+    const {args, flags} = this.parse(WithPiping)
+
+    const name = flags.name || 'world'
+    this.log(`hello ${name} from ${__filename}!`)
+
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
+    }
+  }
+}
+
+export = WithPiping
